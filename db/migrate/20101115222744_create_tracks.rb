4cfdd24edc83571d46e4a32a1f43b41e4b1dfa5d
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-11-18 04:48:27 -0500

    no longer importing data from CSV

--- db/migrate/20101115222744_create_tracks.rb
+++ db/migrate/20101115222744_create_tracks.rb
@@ -1,19 +1,5 @@
 class CreateTracks < ActiveRecord::Migration
 
-  require 'FasterCSV'
-
-  class Track < ActiveRecord::Base
-    def self.import_from_csv data_filename=nil
-      if !data_filename
-        data_filename = "#{RAILS_ROOT}/db/initial_data_tracks.csv"
-      end
-      FasterCSV.foreach(data_filename) do |row|
-        self.create(:title => row[0], :youtube_id => row[1],
-                    :duration => row[2].mmss_to_sec)
-      end
-    end
-  end
-
   def self.up
     create_table :tracks do |t|
       t.string :title, :youtube_id
@@ -21,7 +7,6 @@ class CreateTracks < ActiveRecord::Migration
 
       t.timestamps
     end
-    Track.import_from_csv
   end
 
   def self.down
--- db/migrate/20101115222817_create_samples.rb
+++ db/migrate/20101115222817_create_samples.rb
@@ -1,22 +1,5 @@
 class CreateSamples < ActiveRecord::Migration
 
-  require 'FasterCSV'
-
-  class Sample < ActiveRecord::Base
-    def self.import_from_csv data_filename=nil
-      if !data_filename
-        data_filename = "#{RAILS_ROOT}/db/initial_data_samples.csv"
-      end
-      FasterCSV.foreach(data_filename) do |row|
-        self.create(:track_id => row[0],
-                    :start => row[1].mmss_to_sec,
-                    :stop => row[1].mmss_to_sec + 10,
-                    :title => row[2],
-                    :artist => row[3])
-      end
-    end
-  end
-
   def self.up
     create_table :samples do |t|
       t.string :title, :artist
@@ -25,7 +8,6 @@ class CreateSamples < ActiveRecord::Migration
 
       t.timestamps
     end
-    Sample.import_from_csv
   end
 
   def self.down
