6f674ef8e0e5a8cba90f90553298730593fc820e
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-11-18 04:49:42 -0500

    no longer need fastercsv gem

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,6 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
 
-  config.gem "fastercsv"
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
