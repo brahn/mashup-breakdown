5187335b0bcbd33f703b8d2cdea65b4619593540
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-11-23 12:39:19 -0500

    create feedback page (shudder)

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,6 +1,14 @@
 class ContentController < ApplicationController
 
   def visualizer
+    @meta_title = "Girl Talk - All Day"
+    @title = "All Day by Girl Talk - Mashup Breakdown"
+    @page_class = "viewport-size"
+  end
+
+  def feedback
+    @meta_title = "Feedback"
+    @title = "Feedback / ideas / bug reports"
   end
 
 end
