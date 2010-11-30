c667ee124901758e3d17988fbaea2a28be1e5a76
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-11-29 19:47:24 -0500

    silence bizarro 2.3.2 error following instuctions from http://tommy.chheng.com/index.php/tag/rails-2-3-2/

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,12 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# From http://tommy.chheng.com/index.php/tag/rails-2-3-2/
+module Mime
+  class Type
+    def split(*args)
+      to_s.split(*args)
+    end
+  end
+end
