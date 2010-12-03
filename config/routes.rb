ca710e4573a756555722a40856ed3d3c61bb600f
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-12-03 14:44:05 -0500

    temp url for mass.rsrction.7

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "content", :action => "visualizer"
 
+  map.connect 'temp',  :controller => "content", :action => "visualizer", "album_id" => "mass.rsrction.7"
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
