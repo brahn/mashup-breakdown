65cd0a3509755481a378000396fc12d96fa556d6
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-12-07 15:55:24 -0500

    root defaults to album_id='all-day'

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "content", :action => "visualizer"
+  map.root :controller => "content", :action => "visualizer", :album_id => "all-day"
   map.connect 'index', :controller => "content", :action => "visualizer",
     :show_album_index => "true"
   map.connect ':album_id', :controller => "content", :action => "visualizer"
