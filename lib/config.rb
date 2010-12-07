25c698ae711d36bdcfd97d710de8e2445421c024
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-12-07 02:14:34 -0500

    use flag in lib/config to set whether link to other artists/albums is visible

--- lib/config.rb
+++ lib/config.rb
@@ -1,2 +1,8 @@
 GOOGLE_ANALYTICS_ID = "UA-19852315-1"
 BLOG_URL_BASE = "http://blog.mashupbreakdown.com"
+
+if RAILS_ENV == "production"
+  SHOW_ARTISTS_AND_ALBUMS = false
+else
+  SHOW_ARTISTS_AND_ALBUMS = true
+end
