6bc3e236fafc76dc29cd22559e76aadfede9b7f9
Benjamin Rahn <benjamin.rahn@gmail.com>
2010-12-29 14:09:45 -0500

    import rough data for uspop2010

---
+++ public/javascripts/uspop2010-temp-import.js
@@ -0,0 +1,235 @@
+var uspop2010Import = [{start:8, end:16, artist: "Ke$ha", title: "Tik Tok"},
+  {start:20, end:24, artist: "Ke$ha", title: "Tik Tok"},
+  {start:34, end:36, artist: "Ke$ha", title: "Tik Tok"},
+  {start:59, end:60, artist: "Ke$ha", title: "Tik Tok"},
+  {start:67, end:68, artist: "Ke$ha", title: "Tik Tok"},
+  {start:83, end:89, artist: "Ke$ha", title: "Tik Tok"},
+  {start:91, end:93, artist: "Ke$ha", title: "Tik Tok"},
+  {start:97, end:99, artist: "Ke$ha", title: "Tik Tok"},
+  {start:110, end:115, artist: "Ke$ha", title: "Tik Tok"},
+  {start:157, end:158, artist: "Ke$ha", title: "Tik Tok"},
+  {start:161, end:163, artist: "Ke$ha", title: "Tik Tok"},
+  {start:166, end:168, artist: "Ke$ha", title: "Tik Tok"},
+  {start:173, end:174, artist: "Ke$ha", title: "Tik Tok"},
+  {start:180, end:182, artist: "Ke$ha", title: "Tik Tok"},
+  {start:185, end:189, artist: "Ke$ha", title: "Tik Tok"},
+  {start:206, end:210, artist: "Ke$ha", title: "Tik Tok"},
+  
+ {start:35, end:37, artist: "Lady Antebellum", title: "Need You Now"},
+  {start:71, end:76, artist: "Lady Antebellum", title: "Need You Now"},
+  {start:147, end:150, artist: "Lady Antebellum", title: "Need You Now"},
+  
+ {start:23, end:24, artist: "Train", title: "Hey, Soul Sister"},
+  {start:98, end:102, artist: "Train", title: "Hey, Soul Sister"},
+  {start:166, end:167, artist: "Train", title: "Hey, Soul Sister"},
+  {start:176, end:177, artist: "Train", title: "Hey, Soul Sister"},
+  {start:214, end:215, artist: "Train", title: "Hey, Soul Sister"},
+
+ {start:43, end:46, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:48, end:50, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:58, end:63, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:69, end:71, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:125, end:129, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:152, end:153, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  
+ {start:78, end:83, artist: "Usher ft. will.i.am", title: "OMG"},
+  {start:139, end:145, artist: "Usher ft. will.i.am", title: "OMG"},
+  {start:159, end:161, artist: "Usher ft. will.i.am", title: "OMG"},
+  {start:195, end:197, artist: "Usher ft. will.i.am", title: "OMG"},
+  
+ {start:123, end:125, artist: "B.o.B ft. Hayley Williams", title: "Airplanes"},
+  
+ {start:57, end:58, artist: "Eminem ft. Rihanna", title: "Love the Way You Lie"},
+  
+ {start:53, end:57, artist: "Lady Gaga", title: "Bad Romance"},
+  {start:121, end:122, artist: "Lady Gaga", title: "Bad Romance"},
+  {start:135, end:138, artist: "Lady Gaga", title: "Bad Romance"},
+  {start:163, end:165, artist: "Lady Gaga", title: "Bad Romance"},
+  {start:167, end:169, artist: "Lady Gaga", title: "Bad Romance"},
+  {start:200, end:204, artist: "Lady Gaga", title: "Bad Romance"},
+  
+ {start:15, end:21, artist: "Taio Cruz", title: "Dynamite"},
+  {start:22, end:24, artist: "Taio Cruz", title: "Dynamite"},
+  {start:36, end:39, artist: "Taio Cruz", title: "Dynamite"},
+  {start:46, end:49, artist: "Taio Cruz", title: "Dynamite"},
+  {start:51, end:53, artist: "Taio Cruz", title: "Dynamite"},
+  {start:93, end:97, artist: "Taio Cruz", title: "Dynamite"},
+  {start:101, end:102, artist: "Taio Cruz", title: "Dynamite"},
+  {start:108, end:109, artist: "Taio Cruz", title: "Dynamite"},
+  {start:122, end:123, artist: "Taio Cruz", title: "Dynamite"},
+  {start:128, end:131, artist: "Taio Cruz", title: "Dynamite"},
+  {start:156, end:157, artist: "Taio Cruz", title: "Dynamite"},
+  {start:158, end:160, artist: "Taio Cruz", title: "Dynamite"},
+  {start:168, end:169, artist: "Taio Cruz", title: "Dynamite"},
+  {start:171, end:172, artist: "Taio Cruz", title: "Dynamite"},
+  {start:176, end:177, artist: "Taio Cruz", title: "Dynamite"},
+  {start:187, end:189, artist: "Taio Cruz", title: "Dynamite"},
+  {start:199, end:200, artist: "Taio Cruz", title: "Dynamite"},
+  {start:214, end:215, artist: "Taio Cruz", title: "Dynamite"},
+
+ 
+ {start:61, end:65, artist: "B.o.B ft. Bruno Mars", title: "Nothin' on You"},
+  {start:149, end:153, artist: "B.o.B ft. Bruno Mars", title: "Nothin' on You"},
+  {start:154, end:156, artist: "B.o.B ft. Bruno Mars", title: "Nothin' on You"},
+  
+ {start:39, end:40, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:50, end:51, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:81, end:82, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:89, end:90, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:114, end:115, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:145, end:148, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:155, end:156, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:164, end:165, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:169, end:171, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:184, end:185, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:186, end:188, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:212, end:214, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+ 
+ {start:66, end:67, artist: "Young Money ft. Lloyd", title: "BedRock"},
+  {start:204, end:206, artist: "Young Money ft. Lloyd", title: "BedRock"},
+  {start:207, end:209, artist: "Young Money ft. Lloyd", title: "BedRock"},
+  
+ {start:28, end:32, artist: "Jason Derülo", title: "In My Head"},
+  
+ {start:50, end:52, artist: "Rihanna", title: "Rude Boy"},
+  {start:64, end:67, artist: "Rihanna", title: "Rude Boy"},
+  {start:71, end:72, artist: "Rihanna", title: "Rude Boy"},
+  {start:101, end:109, artist: "Rihanna", title: "Rude Boy"},
+  {start:141, end:142, artist: "Rihanna", title: "Rude Boy"},
+  {start:176, end:180, artist: "Rihanna", title: "Rude Boy"},
+  {start:181, end:183, artist: "Rihanna", title: "Rude Boy"},
+  
+ {start:25, end:26, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:81, end:82, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:84, end:85, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:97, end:98, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:171, end:173, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  
+ {start:68, end:70, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:89, end:91, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:114, end:119, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:127, end:129, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:137, end:140, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:164, end:166, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:182, end:184, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:189, end:195, artist: "Katy Perry", title: "Teenage Dream"},
+  
+ 
+ {start:40, end:41, artist: "Mike Posner", title: "Cooler Than Me"},
+  {start:67, end:68, artist: "Mike Posner", title: "Cooler Than Me"},
+  {start:174, end:176, artist: "Mike Posner", title: "Cooler Than Me"},
+  
+ {start:16, end:18, artist: "The Black Eyed Peas", title: "Imma Be"},
+  {start:78, end:79, artist: "The Black Eyed Peas", title: "Imma Be"},
+  {start:131, end:133, artist: "The Black Eyed Peas", title: "Imma Be"},
+  
+ {start:26, end:29, artist: "Jay-Z ft. Alicia Keys", title: "Empire State of Mind"},
+  
+ {start:11, end:14, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:18, end:22, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:41, end:43, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:118, end:121, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:133, end:136, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:214, end:215, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+
+
+ {start:22, end:25, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:75, end:79, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:85, end:86, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:188, end:190, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:203, end:207, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:209, end:213, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  
+ {start:31, end:35, artist: "Eminem", title: "Not Afraid"},
+  {start:152, end:154, artist: "Eminem", title: "Not Afraid"},
+  
+ {start:213, end:214, artist: "Iyaz", title: "Replay"},
+ 
+ {start:227, end:228, artist: "Ke$ha", title: "Tik Tok"},
+  {start:236, end:238, artist: "Ke$ha", title: "Tik Tok"},
+  {start:241, end:242, artist: "Ke$ha", title: "Tik Tok"},
+  {start:261, end:268, artist: "Ke$ha", title: "Tik Tok"},
+  {start:277, end:279, artist: "Ke$ha", title: "Tik Tok"},
+  {start:289, end:292, artist: "Ke$ha", title: "Tik Tok"},
+  {start:296, end:298, artist: "Ke$ha", title: "Tik Tok"},
+  {start:303, end:304, artist: "Ke$ha", title: "Tik Tok"},
+  
+ {start:307, end:310, artist: "Lady Antebellum", title: "Need You Now"},
+  
+ {start:221, end:222, artist: "Train", title: "Hey, Soul Sister"},
+  {start:249, end:252, artist: "Train", title: "Hey, Soul Sister"},
+  {start:279, end:280, artist: "Train", title: "Hey, Soul Sister"},
+  
+ {start:221, end:222, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:228, end:230, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:285, end:289, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:297, end:299, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  {start:300, end:303, artist: "Katy Perry ft. Snoop Dogg", title: "California Gurls"},
+  
+ {start:219, end:221, artist: "Usher ft. will.i.am", title: "OMG"},
+  {start:234, end:237, artist: "Usher ft. will.i.am", title: "OMG"},
+  
+ 
+ {start:243, end:246, artist: "Eminem ft. Rihanna", title: "Love the Way You Lie"},
+  
+ {start:260, end:263, artist: "Lady Gaga", title: "Bad Romance"},
+  
+ {start:230, end:234, artist: "Taio Cruz", title: "Dynamite"},
+  {start:268, end:269, artist: "Taio Cruz", title: "Dynamite"},
+  {start:270, end:271, artist: "Taio Cruz", title: "Dynamite"},
+  {start:272, end:276, artist: "Taio Cruz", title: "Dynamite"},
+  {start:290, end:292, artist: "Taio Cruz", title: "Dynamite"},
+  {start:301, end:302, artist: "Taio Cruz", title: "Dynamite"},
+  {start:305, end:308, artist: "Taio Cruz", title: "Dynamite"},
+  
+ {start:220, end:221, artist: "Taio Cruz ft. Ludacris", title: "Break Your Heart"},
+  {start:304, end:305, artist: "Taio Cruz ft. Ludacris", title: "Break Your Heart"},
+  {start:308, end:309, artist: "Taio Cruz ft. Ludacris", title: "Break Your Heart"},
+  
+ {start:315, end:322, artist: "B.o.B ft. Bruno Mars", title: "Nothin' on You"},
+  
+ {start:279, end:280, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  {start:309, end:311, artist: "Enrique Iglesias ft. Pitbull", title: "I Like It"},
+  
+ {start:244, end:245, artist: "Young Money ft. Lloyd", title: "BedRock"},
+  {start:271, end:272, artist: "Young Money ft. Lloyd", title: "BedRock"},
+  
+ 
+ {start:224, end:225, artist: "Rihanna", title: "Rude Boy"},
+  {start:251, end:258, artist: "Rihanna", title: "Rude Boy"},
+  
+ {start:240, end:242, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:245, end:249, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:266, end:267, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:268, end:271, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:272, end:274, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:276, end:278, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:299, end:300, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  {start:303, end:305, artist: "Lady Gaga ft. Beyoncé", title: "Telephone"},
+  
+ {start:248, end:250, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:256, end:258, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:267, end:268, artist: "Katy Perry", title: "Teenage Dream"},
+  {start:292, end:293, artist: "Katy Perry", title: "Teenage Dream"},
+  
+ {start:293, end:296, artist: "Bruno Mars", title: "Just the Way You Are"},
+  {start:310, end:319, artist: "Bruno Mars", title: "Just the Way You Are"},
+  
+{start:215, end:217, artist: "Mike Posner", title: "Cooler Than Me"},
+  {start:278, end:279, artist: "Mike Posner", title: "Cooler Than Me"},
+  
+ {start:241, end:244, artist: "The Black Eyed Peas", title: "Imma Be"},
+  {start:258, end:260, artist: "The Black Eyed Peas", title: "Imma Be"},
+  
+ {start:281, end:285, artist: "Jay-Z ft. Alicia Keys", title: "Empire State of Mind"},
+  
+ {start:222, end:229, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  {start:263, end:265, artist: "Usher ft. Pitbull", title: "DJ Got Us Fallin' in Love"},
+  
+ {start:217, end:220, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:238, end:240, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  {start:295, end:297, artist: "Travie McCoy ft. Bruno Mars", title: "Billionaire"},
+  
+ {start:274, end:275, artist: "Eminem", title: "Not Afraid"},
+  {start:280, end:281, artist: "Eminem", title: "Not Afraid"}
+]
