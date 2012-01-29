c0421b24785614ca063fecce935c30b4de61162b
Benjamin Rahn <brahn@blueleaf.com>
2012-01-28 22:38:42 -0500

    let's see what happens if we commit this stuff and push it to Heroku

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+gem 'hoptoad_notifier'
+gem 'pg'
+gem 'rails', '2.3.11'
+gem 'heroku'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
 
-  config.gem 'hoptoad_notifier'
+  # config.gem 'hoptoad_notifier'
 
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
